06b7f2440d7fb17041a234e44b32c0ce37865d8c
Girish <girish@post.com>
2025-01-04 11:16:54 -0800

    Enhance CI workflow by adding Puppeteer and Karma configuration for headless testing

---
+++ src/karma.conf.js
@@ -0,0 +1,57 @@
+// Karma configuration file, see link for more information
+
+const { clear } = require('console')
+const { single } = require('rxjs')
+
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/getting-started-angular-tdd'),
+      subdir: '.',
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+        { type: 'text-summary', subdir: 'report-text' },
+        { type: 'lcov', subdir: 'report-lcov' },
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
+    singleRun: false,
+  })
+}
